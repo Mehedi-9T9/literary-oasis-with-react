ed91537671b39c5d21acb8b8304f8b603072c3f1
Mehedi-9T9 <mehedi.hasan9t9@gmail.com>
2024-03-27 15:45:31 +0600

    chat added complate

--- literary-oasis-with-react-js/src/Components/PageToRead/PageToRead.jsx
+++ literary-oasis-with-react-js/src/Components/PageToRead/PageToRead.jsx
@@ -1,9 +1,84 @@
-import React from 'react';
+
+import { useEffect, useState } from 'react';
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts';
+import { getBookId } from '../../utility/utility';
+
+
+
 
 const PageToRead = () => {
+    const [chatData, setChatData] = useState([])
+    useEffect(() => {
+        fetch('../books.json')
+            .then(res => res.json())
+            .then(data => {
+                const storedBookId = getBookId()
+                // console.log(storedBookId, data);
+                if (data.length > 0) {
+                    const booksArray = [];
+                    for (const id of storedBookId) {
+                        const book = data.find(item => item.bookId === id)
+                        if (book) {
+                            booksArray.push(book)
+                        }
+                        // console.log(book);
+                        // setChatData(book);
+                    }
+                    setChatData(booksArray);
+                }
+
+            })
+
+
+    }, [])
+
+
+
+
+
+    const colors = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', 'red', 'pink'];
+
+
+
+    const getPath = (x, y, width, height) => {
+        return `M${x},${y + height}C${x + width / 3},${y + height} ${x + width / 2},${y + height / 3}
+  ${x + width / 2}, ${y}
+  C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${y + height} ${x + width}, ${y + height}
+  Z`;
+    };
+
+    const TriangleBar = (props) => {
+        const { fill, x, y, width, height } = props;
+
+        return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
+    };
+
     return (
-        <div>
-            <h2>page to read</h2>
+        <div className='container mx-auto bg-[#EEEEEE] mt-10 rounded-xl p-10'>
+
+            <BarChart
+                width={1200}
+                height={400}
+                data={chatData}
+                margin={{
+                    top: 20,
+                    right: 30,
+                    left: 20,
+                    bottom: 5,
+                }}
+            >
+                <Tooltip />
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="bookName" />
+                <YAxis />
+                <Bar dataKey="totalPages" fill="#8884d8" shape={<TriangleBar />} label={{ position: 'top' }}>
+                    {chatData.map((entry, index) => (
+                        <Cell key={`cell-${index}`} fill={colors[index % 20]} />
+                    ))}
+                </Bar>
+            </BarChart>
+
+
         </div>
     );
 };
